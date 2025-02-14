12a43619b4192e551fcdd7e885af2d5d1c2bb938
Abdul Rehman <bilalshuja77@gmail.com>
2025-02-14 09:31:38 +0500

    Enable / Disable comments - Reactions / Like count - Edit post

--- src/app/pages/page/Modal/PostPollModal.js
+++ src/app/pages/page/Modal/PostPollModal.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { toast } from "react-toastify";
 import createAPI from "@/app/lib/axios";
+import React, {useState } from 'react';
 
 export default function PostPollModal({ pollModal, setPollModal , posts ,setPosts , myPageTimeline , endpoint}) {
 
