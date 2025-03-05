374c7a3e50af57cbacec8028fc2ce5fc479105a5
Abdul Rehman <bilalshuja77@gmail.com>
2025-03-05 08:58:36 +0500

    new changes

--- src/app/pages/savedposts/page.js
+++ src/app/pages/savedposts/page.js
@@ -19,6 +19,8 @@ import SharePostTimelineModal from "../Modals/SharePostTimelineModal";
 import SharedPosts from "../components/sharedPosts";
 import { useRouter } from "next/navigation";
 import { useSiteSettings } from "@/context/SiteSettingsContext"
+import ReadMoreLess from 'react-read-more-less';
+
 
 export default function Savedposts() {
 
